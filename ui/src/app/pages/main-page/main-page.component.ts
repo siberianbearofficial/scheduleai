abf9f62b4ffb41f42ae00793ddb128360fa67e8c
k3ssler <jkessler365@mail.ru>
2025-04-19 12:58:48 +0300

    structure

---
+++ ui/src/app/pages/main-page/main-page.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import SearchBarComponent from '../../components/ai-search-bar/ai-search-bar.component';
+
+@Component({
+  selector: 'app-main-page',
+  imports: [
+    SearchBarComponent
+  ],
+  templateUrl: './main-page.component.html',
+  styleUrl: './main-page.component.scss'
+})
+
+export class MainPageComponent {
+
+}
