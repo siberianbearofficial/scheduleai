1c58bec286f0d08ae60abef438b373c4ede12a6a
SergeiKrivko <sergik.krivko@gmail.com>
2025-04-19 16:01:24 +0300

    IAiHelperService

--- bff/ScheduleAI.Api/ScheduleAI.Api/Controllers/AiHelperController.cs
+++ bff/ScheduleAI.Api/ScheduleAI.Api/Controllers/AiHelperController.cs
@@ -1,17 +1,26 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using ScheduleAI.Api.Schemas;
+using ScheduleAI.Core.Abstractions;
 
 namespace ScheduleAI.Api.Controllers;
 
 [ApiController]
 [Route("/api/v1/aiHelper")]
-public class AiHelperController : Controller
+public class AiHelperController(IAiHelperService aiHelperService) : Controller
 {
     [HttpPost]
     public async Task<ActionResult<ResponseSchema<AiHelperResponseModel>>> PostAiHelperPrompt(
         [FromBody] [Required] AiHelperRequestModel request)
     {
-        throw new NotImplementedException();
+        var answer = await aiHelperService.AskHelper(request.Prompt, request.UniversityId, request.GroupId);
+        return Ok(new ResponseSchema<AiHelperResponseModel>
+        {
+            Detail = "Answer from AiHelper",
+            Data = new AiHelperResponseModel()
+            {
+                Answer = answer,
+            }
+        });
     }
 }
