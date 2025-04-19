30635a30e7b83f9edd99dc2d43b054ffba599f04
k3ssler <jkessler365@mail.ru>
2025-04-19 19:08:48 +0300

    tuiBadge & tuiStatus

--- ui/src/app/components/pair/pair.component.ts
+++ ui/src/app/components/pair/pair.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {TuiBadge, TuiStatus} from '@taiga-ui/kit'
 
 @Component({
   selector: 'app-pair',
-  imports: [],
+  imports: [
+    TuiBadge,
+    TuiStatus
+  ],
   templateUrl: './pair.component.html',
   styleUrl: './pair.component.scss'
 })
