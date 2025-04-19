8c7239612173fc5fc71ba937912b33f6795cb4f8
SergeiKrivko <121343672+SergeiKrivko@users.noreply.github.com>
2025-04-19 18:55:18 +0300

    Merged schedule (#4)

--- bff/ScheduleAI.Api/ScheduleAI.Api/Controllers/PairsController.cs
+++ bff/ScheduleAI.Api/ScheduleAI.Api/Controllers/PairsController.cs
@@ -1,17 +1,25 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using ScheduleAI.Api.Schemas;
+using ScheduleAI.Core.Abstractions;
 using ScheduleAI.Core.Models;
 
 namespace ScheduleAI.Api.Controllers;
 
 [ApiController]
 [Route("/api/v1/mergedPairs")]
-public class PairsController : Controller
+public class PairsController(IScheduleService scheduleService) : Controller
 {
     [HttpPost]
-    public async Task<ActionResult<ResponseSchema<MergedPair[]>>> MergedPairs([FromBody] [Required] MergedPairsRequestSchema request)
+    public async Task<ActionResult<ResponseSchema<MergedPair[]>>> MergedPairs(
+        [FromBody] [Required] MergedPairsRequestSchema request)
     {
-        throw new NotImplementedException();
+        var mergedPairs = await scheduleService.GetMergedScheduleAsync(request.UniversityId, request.GroupId,
+            request.TeacherId, request.StartTime, request.EndTime);
+        return Ok(new ResponseSchema<MergedPair[]>
+        {
+            Detail = "Pairs was merged",
+            Data = mergedPairs.ToArray()
+        });
     }
 }
