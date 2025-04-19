ba104bcb14c2fd17c8008bfe7432b92af69bac8f
SergeiKrivko <sergik.krivko@gmail.com>
2025-04-19 19:41:07 +0300

    MergedPairsService

--- ui/src/app/pages/merged-pairs-page/merged-pairs-page.component.ts
+++ ui/src/app/pages/merged-pairs-page/merged-pairs-page.component.ts
@@ -1,11 +1,12 @@
 import {Component, inject} from '@angular/core';
 import {TeacherCardComponent} from '../../components/teacher-card/teacher-card.component';
 import {AsyncPipe} from '@angular/common';
-import {TeacherService} from '../../services/groups.service';
+import {TeacherService} from '../../services/teachers.service';
 import {LogoComponent} from '../../components/logo/logo.component';
 import FooterComponent from '../../components/footer/footer.component';
 import TeacherSearchBarComponent from '../../components/teacher-search-bar/teacher-search-bar.component';
 import {RouterLink} from '@angular/router';
+import {MergedPairsService} from '../../services/merged-pairs.service';
 
 @Component({
   selector: 'app-merged-pairs-page',
@@ -21,7 +22,7 @@ import {RouterLink} from '@angular/router';
   styleUrl: './merged-pairs-page.component.scss'
 })
 export class MergedPairsPageComponent {
-  private readonly teacherService: TeacherService = inject(TeacherService);
+  private readonly mergedPairsService: MergedPairsService = inject(MergedPairsService);
 
-  readonly teachers$ = this.teacherService.teachers$;
+  readonly merged_pairs$ = this.mergedPairsService.merged_pairs$;
 }
