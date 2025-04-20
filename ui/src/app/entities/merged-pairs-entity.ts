55f6535b96aab803ca835053de120ec8581b98cb
SergeiKrivko <sergik.krivko@gmail.com>
2025-04-20 09:13:06 +0300

    refactor

--- ui/src/app/entities/merged-pairs-entity.ts
+++ ui/src/app/entities/merged-pairs-entity.ts
@@ -2,14 +2,14 @@ import {Moment} from 'moment';
 import {PairEntity} from './pair-entity';
 
 export interface MergedPairEntity {
-  start_time: Moment;
-  end_time: Moment;
-  act_type: string;
+  startTime: Moment;
+  endTime: Moment;
+  actType: string;
   discipline: string;
   rooms: string[];
 
   convenience: number;
-  wait_time_sec: number;
+  waitTimeSec: number;
   status: 'beforePairs' | 'afterPairs' | 'inGap' | 'collision';
   collisions: PairEntity[];
 }
