96031cac7196379c38ce2bf3c5e5147ea81c88b3
k3ssler <jkessler365@mail.ru>
2025-04-20 09:57:36 +0300

    teacher card added

--- ui/src/app/pages/teacher-page/teacher-page.component.ts
+++ ui/src/app/pages/teacher-page/teacher-page.component.ts
@@ -1,37 +1,49 @@
-import {Component, inject, input, InputSignal, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, OnInit} from '@angular/core';
 import {LogoComponent} from '../../components/logo/logo.component';
 import FooterComponent from '../../components/footer/footer.component';
 import {ActivatedRoute, Router} from '@angular/router';
 import {MergedPairsService} from '../../services/merged-pairs.service';
 import {PairComponent} from '../../components/pair/pair.component';
 import {AsyncPipe} from '@angular/common';
-import {TuiButton} from '@taiga-ui/core';
+import {TuiAppearance, TuiButton, TuiTitle} from '@taiga-ui/core';
+import {TeacherService} from '../../services/groups.service';
+import {BehaviorSubject, EMPTY, switchMap} from 'rxjs';
+import {TuiCardLarge, TuiHeader} from '@taiga-ui/layout';
+import {TuiBadge} from '@taiga-ui/kit';
 
 @Component({
+  standalone: true,
   selector: 'app-teacher-page',
   imports: [
     LogoComponent,
     FooterComponent,
     PairComponent,
     AsyncPipe,
-    TuiButton
+    TuiButton,
+    TuiAppearance,
+    TuiCardLarge,
+    TuiHeader,
+    TuiTitle,
+    TuiBadge
   ],
   templateUrl: './teacher-page.component.html',
-  styleUrl: './teacher-page.component.scss'
+  styleUrl: './teacher-page.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TeacherPageComponent implements OnInit {
-  teacherId: string | null = null;
+  private readonly teacherService = inject(TeacherService);
+  private readonly router = inject(Router);
+  private readonly route = inject(ActivatedRoute);
 
-  constructor(
-    private router: Router,
-    private route: ActivatedRoute
-  ) {}
+  private readonly teacherId$$ = new BehaviorSubject<string | null>(null);
+  protected readonly teacher$ = this.teacherId$$.pipe(
+    switchMap(id => id ? this.teacherService.teacherById(id) : EMPTY)
+  );
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
-      this.teacherId = params['teacherId'];
-      console.log('Teacher ID:', this.teacherId);
-      // Можно сделать запрос на сервер с этим ID, например, получить информацию о преподавателе
+      const teacherId = params['teacherId'] as string | null;
+      this.teacherId$$.next(teacherId);
     });
   }
 
