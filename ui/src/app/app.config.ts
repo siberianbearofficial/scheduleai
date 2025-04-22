f0ae4d2a62347cca61c4df55f5b5667e40aecf05
SergeiKrivko <sergik.krivko@gmail.com>
2025-04-22 19:57:49 +0300

    switch to deployed bff

--- ui/src/app/app.config.ts
+++ ui/src/app/app.config.ts
@@ -9,7 +9,7 @@ import {API_BASE_URL} from './bff-client/bff-client';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    { provide: API_BASE_URL, useValue: "http://localhost:5000" },
+    { provide: API_BASE_URL, useValue: "https://scheduleai-bff.nachert.art" },
     provideAnimations(),
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
