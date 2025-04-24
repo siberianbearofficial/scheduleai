61201eb6c972e7ee0ce3a829728edc6b3c0db037
SergeiKrivko <121343672+SergeiKrivko@users.noreply.github.com>
2025-04-24 18:28:55 +0300

    Chat (#18)

--- ui/src/app/pages/chat-page/chat-page.component.ts
+++ ui/src/app/pages/chat-page/chat-page.component.ts
@@ -1,17 +1,17 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, DestroyRef, inject, OnInit} from '@angular/core';
 import {TuiMessage} from '@taiga-ui/kit';
+import {LogoComponent} from '../../components/logo/logo.component';
 import FooterComponent from '../../components/footer/footer.component';
 import {HeaderComponent} from '../../components/header/header.component';
-import {DatePipe} from '@angular/common';
+import {AsyncPipe, DatePipe} from '@angular/common';
 import SimpleSearchBarComponent from '../../components/search-bar/search-bar.component'
-import {FormsModule} from '@angular/forms';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {ActivatedRoute} from '@angular/router';
-
-interface Message {
-  text: string;
-  sender: 'user' | 'server';
-  timestamp: Date;
-}
+import {ChatService} from '../../services/chat.service';
+import {first, take} from 'rxjs';
+import {MessageRole} from '../../entities/message-entity';
+import {TuiTextfieldComponent, TuiTextfieldDirective} from '@taiga-ui/core';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 
 @Component({
   standalone: true,
@@ -20,60 +20,50 @@ interface Message {
   imports: [
     TuiMessage,
     FormsModule,
+    LogoComponent,
     FooterComponent,
     HeaderComponent,
     DatePipe,
-    SimpleSearchBarComponent
+    SimpleSearchBarComponent,
+    AsyncPipe,
+    TuiTextfieldComponent,
+    TuiTextfieldDirective,
+    ReactiveFormsModule
   ],
   templateUrl: './chat-page.component.html',
   styleUrl: './chat-page.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ChatPageComponent {
-  messages: Message[] = [];
-  newMessage = '';
+export class ChatPageComponent implements OnInit {
+  private readonly chatService: ChatService = inject(ChatService);
+  private readonly route: ActivatedRoute = inject(ActivatedRoute);
+  private readonly destroyRef: DestroyRef = inject(DestroyRef);
 
-  constructor(private route: ActivatedRoute) {}
+  protected readonly messageInputControl = new FormControl('');
+
+  protected readonly messages$ = this.chatService.messages$;
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
       if (params['message']) {
-        const userMessage = params['message'];
-        this.addMessage(userMessage, 'user');
-        this.addMessage(this.getServerResponse(userMessage), 'server');
+        this.sendMessage(params['message']);
       }
     });
   }
 
-  sendMessage() {
-    if (!this.newMessage.trim()) return;
+  protected readonly MessageRole = MessageRole;
 
-    // Добавляем сообщение пользователя
-    this.addMessage(this.newMessage, 'user');
-    const userMessage = this.newMessage;
-    this.newMessage = '';
-    const response = this.getServerResponse(userMessage);
-    this.addMessage(response, 'server');
+  private sendMessage(message: string): void {
+    this.chatService.sendMessage(message).pipe(
+      first(),
+      takeUntilDestroyed(this.destroyRef),
+    ).subscribe();
   }
 
-  private addMessage(text: string, sender: 'user' | 'server') {
-    this.messages.push({
-      text,
-      sender,
-      timestamp: new Date()
-    });
-  }
-
-  private getServerResponse(userMessage: string): string {
-    const responses = [
-      "Я получил ваше сообщение: " + userMessage,
-      "Интересный вопрос!",
-      "Давайте обсудим это подробнее.",
-      "Спасибо за ваше сообщение!",
-      "Я обрабатываю ваш запрос..."
-    ];
-
-    return responses[Math.floor(Math.random() * responses.length)];
+  protected onSubmit() {
+    if (this.messageInputControl.value)
+      this.sendMessage(this.messageInputControl.value)
+    this.messageInputControl.setValue("");
   }
 }
 
