54c1e7410ef624a632754990eaeb1649deb60bdc
SergeiKrivko <sergik.krivko@gmail.com>
2025-04-28 13:50:41 +0300

    remove test data

--- ui/src/app/services/chat.service.ts
+++ ui/src/app/services/chat.service.ts
@@ -22,39 +22,7 @@ export class ChatService {
   private readonly groupsService: GroupsService = inject(GroupsService);
 
   private readonly messages$$ = signalState<MessagesStore>({
-    messages: [
-      {
-        role: MessageRole.User,
-        text: "Во сколько завтра я могу встретиться с Куровым?",
-        timestamp: moment(),
-        pairs: [],
-      },
-      {
-        role: MessageRole.Assistant,
-        text: "Вы можете встретиться с <b>Куровым Андреем Владимировичем</b>, завтра, 29 апреля:<ul>  <li><b>с 12:05 до 13:05</b>, лекция <i>«Базы Данных»</i>, аудитория <b>102x</b></li>  <li><b>с 15:40 до 16:10</b>, семинар <i>«Компьютерная Графика»</i>, аудитория <b>304x</b></li></ul>",
-        timestamp: moment(),
-        pairs: [
-          {
-            "teachers": ["Куров Андрей Владимирович"],
-            "groups": ["Группа 101", "Группа 102"],
-            "startTime": moment("2025-04-27T12:05:00"),
-            "endTime": moment("2025-04-27T13:05:00"),
-            "rooms": ["102x"],
-            "discipline": "Базы данных",
-            "actType": "Лекция",
-            "convenience": {"coefficient": 0.5, "status": 0, waitTime: duration(0), collisions: []}
-          }, {
-            "teachers": ["Куров Андрей Владимирович"],
-            "groups": ["Группа 103"],
-            "startTime": moment("2025-04-27T15:40:00"),
-            "endTime": moment("2025-04-27T16:10:00"),
-            "rooms": ["304x"],
-            "discipline": "Компьютерная графика",
-            "actType": "Семинар",
-            "convenience": {"coefficient": 0.7, "status": 0, waitTime: duration(0), collisions: []}
-          }],
-      },
-    ]
+    messages: [],
   });
 
   readonly messages$ = toObservable(this.messages$$.messages);
