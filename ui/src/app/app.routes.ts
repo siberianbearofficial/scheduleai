258b93e08d45ac1aa975611da205cc00c5c9f1c8
SergeiKrivko <sergik.krivko@gmail.com>
2025-06-22 22:12:28 +0300

    remove teachers page

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
 import {MainPageComponent} from './pages/main-page/main-page.component';
 import {MergedPairsPageComponent} from './pages/merged-pairs-page/merged-pairs-page.component';
-import {TeacherPageComponent} from './pages/teacher-page/teacher-page.component';
 import {ChatPageComponent} from './pages/chat-page/chat-page.component';
 
 export const routes: Routes = [
   { path: '', component: MainPageComponent },
-  { path: 'teacherSchedule', component: TeacherPageComponent},
   { path: 'teacherSchedule/:teacherId', component: MergedPairsPageComponent},
   { path: 'chat', component: ChatPageComponent },
   { path: '**', redirectTo: '/', pathMatch: 'full' },
