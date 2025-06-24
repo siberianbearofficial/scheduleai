5b8dc517881e117e5435c52c1bd4ab7d582b7821
SergeiKrivko <sergik.krivko@gmail.com>
2025-06-24 18:42:37 +0300

    Remove Bmstu.Mock

--- bff/ScheduleAI.Api/Universities/Bmstu.Mock/Models/Group.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text.Json.Serialization;
-
-namespace Bmstu.Mock.Models;
-
-public class Group
-{
-    [JsonPropertyName("uuid")] public Guid? Id { get; set; }
-    [JsonPropertyName("name")] public string? Name { get; set; }
-}
